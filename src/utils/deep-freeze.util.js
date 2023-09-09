c22095313c3f9bdb4e0ef7d50d37c463186c6c0f
Omar Castro <omar.castro.360@gmail.com>
2023-09-09 03:36:11 +0100

    refactor: refactor deep freeze to js

---
+++ src/utils/deep-freeze.util.js
@@ -0,0 +1,18 @@
+
+/**
+ * @template T
+ * @param {DeepFrozen<T>} x 
+ * @returns {DeepFrozen<T>}
+ */
+export function deepFreeze (x) {
+  Object.freeze(x)
+  x && Object.values(x).filter(x => !Object.isFrozen(x)).forEach(deepFreeze)
+  return x
+}
+
+
+/**
+ * @template T
+ * @typedef {T extends Function | boolean | number | string | null | undefined ? T : T extends Array<infer U> ? ReadonlyArray<DeepFrozen<U>> : T extends Map<infer K, infer V> ? ReadonlyMap<DeepFrozen<K>, DeepFrozen<V>> : T extends Set<infer S> ? ReadonlySet<DeepFrozen<S>> : {readonly [P in keyof T]: DeepFrozen<T[P]>}} DeepFrozen<T>
+ */
+
