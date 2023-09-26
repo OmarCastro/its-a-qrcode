c9146a2c66dbeb563eefdfa15b49e939d5e8100e
Omar Castro <omar.castro.360@gmail.com>
2023-09-26 21:44:32 +0100

    chore: lint docs

--- docs/doc.js
+++ docs/doc.js
@@ -1,46 +1,42 @@
-import {QrCode} from '../src/qr-code.js'
-import {createSvgTag} from '../src/render/svg.render.js'
+import { QrCode } from '../src/qr-code.js'
+import { createSvgTag } from '../src/render/svg.render.js'
 
+function createQrCode (text, typeNumber, errorCorrectionLevel, mode) {
+  const qr = new QrCode(typeNumber || 4, errorCorrectionLevel || 'M')
+  qr.addData(text, mode)
+  qr.make()
 
-function create_qrcode(text, typeNumber, errorCorrectionLevel, mode) {
-  const qr = new QrCode(typeNumber || 4, errorCorrectionLevel || 'M');
-  qr.addData(text, mode);
-  qr.make();
-  
-  return createSvgTag({qrcode: qr});
+  return createSvgTag({ qrcode: qr })
 };
-  
-function update_qrcode() {
-  var form = document.forms['qrForm'];
-  var text = form.elements['msg'].value.replace(/^[\s\u3000]+|[\s\u3000]+$/g, '');
-  var t = form.elements['t'].value;
-  var e = form.elements['e'].value;
-  var m = form.elements['m'].value;
-  var mb = form.elements['mb'].value;
-  document.getElementById('qr').innerHTML = create_qrcode(text, t, e, m, mb);
+
+function updateQrCode () {
+  const form = document.forms.qrForm
+  const text = form.elements.msg.value.replace(/^[\s\u3000]+|[\s\u3000]+$/g, '')
+  const t = form.elements.t.value
+  const e = form.elements.e.value
+  const m = form.elements.m.value
+  const mb = form.elements.mb.value
+  document.getElementById('qr').innerHTML = createQrCode(text, t, e, m, mb)
 };
-  
 
-
-window.addEventListener("click", (e) => {
-    if(e.target.matches(".button--update")){
-        update_qrcode()
-    }
+window.addEventListener('click', (e) => {
+  if (e.target.matches('.button--update')) {
+    updateQrCode()
+  }
 })
 
-
-function crtOpt(value, label) {
-  var opt = document.createElement('option');
-  opt.appendChild(document.createTextNode(label) );
-  opt.value = value;
-  return opt;
+function crtOpt (value, label) {
+  const opt = document.createElement('option')
+  opt.appendChild(document.createTextNode(label))
+  opt.value = value
+  return opt
 };
 
-var t = document.forms['qrForm'].elements['t'];
-t.appendChild(crtOpt('' + 0, 'Auto Detect') );
-for (var i = 1; i <= 40; i += 1) {
-  t.appendChild(crtOpt('' + i, '' + i) );
+const t = document.forms.qrForm.elements.t
+t.appendChild(crtOpt('' + 0, 'Auto Detect'))
+for (let i = 1; i <= 40; i += 1) {
+  t.appendChild(crtOpt('' + i, '' + i))
 }
-t.value = '0';
+t.value = '0'
 
-update_qrcode();
+updateQrCode()
