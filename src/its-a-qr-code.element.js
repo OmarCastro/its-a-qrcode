dc6cefd5688bde86d021cdf98d153f05ae0c2197
Omar Castro <omar.castro.360@gmail.com>
2023-10-02 20:39:31 +0100

    fix: fix text change not updating qr code
    - fix text change not updating qr code
    
    - lint code

--- src/its-a-qr-code.element.js
+++ src/its-a-qr-code.element.js
@@ -1,46 +1,59 @@
-import { QrCode } from "./qr-code";
-import {createSvgTag} from '../src/render/svg.render.js'
-
-
+import { QrCode } from './qr-code'
+import { createSvgTag } from '../src/render/svg.render.js'
 
 export class QRCodeElement extends HTMLElement {
-  
-  constructor(){
+  constructor () {
     super()
+    this.attachShadow({ mode: 'open' })
     observer.observe(this, observerOptions)
   }
-    
+
+  connectedCallback () {
+    applyQrCode(this)
+  }
 }
 
 /** @type {MutationObserverInit} */
 const observerOptions = {
-  characterData: true, 
+  characterData: true,
   characterDataOldValue: true,
-  attributes: true
+  attributes: true,
+  subtree: true,
 }
 const observer = new MutationObserver((records) => {
-  const updatedNodes = new Set(records.map(record => record.target))
+  const updatedNodes = new Set()
+  for (const record of records) {
+    const { target } = record
+    if (target instanceof QRCodeElement) {
+      updatedNodes.add(target)
+    } else if (target.nodeType === Node.TEXT_NODE && target.parentElement instanceof QRCodeElement) {
+      updatedNodes.add(target.parentElement)
+    }
+  }
   updatedNodes.forEach(el => el instanceof QRCodeElement && applyQrCode(el))
 })
 
-
-
 /**
- * 
- * @param {HTMLElement} element 
+ *
+ * @param {HTMLElement} element
  */
-function applyQrCode(element){
+function applyQrCode (element) {
   const typeNumber = 0
   const errorCorrectionLevel = null
 
-  const {textContent} = element
-  if(!textContent){
+  const { shadowRoot } = element
+  if (!shadowRoot) {
     return
   }
 
-  const qr = new QrCode(typeNumber || 4, errorCorrectionLevel || 'M');
-  qr.addData(textContent, "Byte");
-  qr.make();
+  const { textContent } = element
+  if (!textContent) {
+    return
+  }
 
-  element.innerHTML = createSvgTag({qrcode: qr})
+  const qr = new QrCode(typeNumber || 4, errorCorrectionLevel || 'M')
+  qr.addData(textContent, 'Byte')
+  qr.make()
+
+  shadowRoot.innerHTML = createSvgTag({ qrcode: qr })
 }
