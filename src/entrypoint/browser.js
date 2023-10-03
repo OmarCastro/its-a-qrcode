3ad7572553aeb9967b727511143d7b8c9ccc5420
Omar Castro <omar.castro.360@gmail.com>
2023-10-03 22:04:47 +0100

    src: add browser entrypoint

--- src/entrypoint/browser.js
+++ src/entrypoint/browser.js
@@ -1,2 +1,15 @@
-export { QrCode } from "../qr-code";
-export {createSvgTag} from '../render/svg.render.js'
+import { QRCodeElement as Element } from '../its-a-qr-code.element.js'
+export { QrCode } from '../qr-code.js'
+
+const url = new URL(import.meta.url)
+const elementName = url.searchParams.get('named')
+if (elementName) {
+  if (customElements.get(elementName) != null) {
+    console.error(`A custom element with name "${elementName}" already exists`)
+  } else {
+    customElements.define(elementName, Element)
+  }
+}
+
+export const QRCodeElement = Element
+export default QRCodeElement
