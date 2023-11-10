3f448dcf79d624e6d289fe66d4cdf7c2e981d0e1
Omar Castro <omar.castro.360@gmail.com>
2023-11-10 23:26:15 +0000

    doc: make documentation logic skeleton

--- docs/doc.js
+++ docs/doc.js
@@ -1,42 +1,99 @@
-import { QrCode } from '../src/qr-code.js'
-import { createSvgTag } from '../src/render/svg.render.js'
+/**
+ * @param {Record<string, any>} exampleObject - example object data
+ * @param {Element} codeView - element container
+ * @returns {Promise<EditorView>} codemirror EditorView object
+ */
+async function transformCodeViewToEditor (exampleObject, codeView) {
+  const lang = codeView.getAttribute('data-lang')
+  const { createEditorView } = await import('./code-editor.lazy.js')
+  const editorView = createEditorView({
+    doc: codeView.textContent || '',
+    onChange: (e) => {
+      try {
+        const value = e.state.doc.toString()
+        const newTranslations = JSON.parse(value)
+        if (JSON.stringify(newTranslations) !== JSON.stringify(exampleObject[lang])) {
+          exampleObject[lang] = newTranslations
+        }
+      } catch {
+        // ignore
+      }
+    },
+    parent: codeView,
+  })
 
-function createQrCode (text, typeNumber, errorCorrectionLevel, mode) {
-  const qr = new QrCode(typeNumber || 4, errorCorrectionLevel || 'M')
-  qr.addData(text, mode)
-  qr.make()
+  codeView.replaceChildren(editorView.dom)
+  return editorView
+}
 
-  return createSvgTag({ qrcode: qr })
-};
+/**
+ * @param {Record<string, any>} exampleObject - example object data
+ * @param {Element} editorElement - editor element
+ */
+async function applyExample (exampleObject, editorElement) {
+  const exampleContainer = editorElement.closest('.example')
+  if (!exampleContainer) { return }
 
-function updateQrCode () {
-  const form = document.forms.qrForm
-  const text = form.elements.msg.value.replace(/^[\s\u3000]+|[\s\u3000]+$/g, '')
-  const t = form.elements.t.value
-  const e = form.elements.e.value
-  const m = form.elements.m.value
-  const mb = form.elements.mb.value
-  document.getElementById('qr').innerHTML = createQrCode(text, t, e, m, mb)
-};
+  const lang = editorElement.getAttribute('data-lang')
+  if (!lang) { return }
 
-window.addEventListener('click', (e) => {
-  if (e.target.matches('.button--update')) {
-    updateQrCode()
-  }
+  editorElement.addEventListener('click', async function eventListener (event) {
+    const selection = getSelection()
+    if (selection && selection.toString()) {
+      return
+    }
+    editorElement.removeEventListener('click', eventListener)
+    const editorView = await transformCodeViewToEditor(exampleObject, editorElement)
+    const { clientX, clientY } = event
+    requestAnimationFrame(() => {
+      editorView.focus()
+      const anchor = editorView.posAtCoords({ x: clientX, y: clientY })
+      anchor != null && editorView.dispatch({ selection: { anchor } })
+    })
+  })
+}
+
+document.querySelectorAll('.example').forEach(element => {
+  const exampleObj = {}
+
+  console.log('.example %o', element)
+
+  element.querySelectorAll('.example__json .editor').forEach(element => {
+    const lang = element.getAttribute('data-lang')
+    if (!lang) { return }
+    exampleObj[lang] = JSON.parse(element.textContent || '')
+    requestIdleCallback(() => applyExample(exampleObj, element))
+  })
+
+  element.addEventListener('input', (event) => {
+    const bindSelectorAttr = 'data-bind-selector'
+    if (event.target.matches('.lang-edit')) {
+      const selector = event.target.getAttribute(bindSelectorAttr) || '[lang]'
+      const node = element.querySelector(selector)
+      node && node.setAttribute('lang', event.target.textContent)
+    }
+    if (event.target.matches('.data-i18n-edit')) {
+      const selector = event.target.getAttribute(bindSelectorAttr) || '[data-i18n-text]'
+      const node = element.querySelector(selector)
+      node && node.setAttribute('data-i18n-text', event.target.textContent)
+    }
+    if (event.target.matches('.data-i18n--title-edit')) {
+      const selector = event.target.getAttribute(bindSelectorAttr) || '[data-i18n--title]'
+      const node = element.querySelector(selector)
+      node && node.setAttribute('data-i18n--title', event.target.textContent)
+    }
+  })
 })
 
-function crtOpt (value, label) {
-  const opt = document.createElement('option')
-  opt.appendChild(document.createTextNode(label))
-  opt.value = value
-  return opt
-};
-
-const t = document.forms.qrForm.elements.t
-t.appendChild(crtOpt('' + 0, 'Auto Detect'))
-for (let i = 1; i <= 40; i += 1) {
-  t.appendChild(crtOpt('' + i, '' + i))
+/**
+ * @param {Event} event - 'input' event object
+ */
+function reactElementNameChange (event) {
+  const componentName = event.target.closest('.component-name-edit')
+  if (componentName == null) { return }
+  const newText = componentName.textContent
+  document.body.querySelectorAll('.component-name-edit').forEach(ref => { if (componentName !== ref) ref.textContent = newText })
+  document.body.querySelectorAll('.component-name-ref').forEach(ref => { ref.textContent = newText })
 }
-t.value = '0'
 
-updateQrCode()
+document.body.addEventListener('input', (event) => { reactElementNameChange(event) })
