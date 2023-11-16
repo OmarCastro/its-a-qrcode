6c9f6a19d398e20f6500e6bedc32bf67a0cec26e
Omar Castro <omar.castro.360@gmail.com>
2023-11-16 23:54:27 +0000

    src: document utf8-to-jis-table util

--- src/utils/utf8-to-jis-table.js
+++ src/utils/utf8-to-jis-table.js
@@ -1,13 +1,17 @@
 import { base64ToHex } from './text-decode-encode.util.js'
 import UTF8_TO_JIS_TABLE from './utf8-to-jis-table.constants.js'
 
-/** @param {string} x  */
+/** @param {string} x - hex string */
 const xInt = x => parseInt(x, 16)
-/** @param {string} x  */
+/** @param {string} x - base64 string */
 const b64Int = x => xInt(base64ToHex(x))
 
+/** @typedef {{[key: string]: string}} CompressedTable */
+/** @typedef {{[utf8Value: number]: number}} Utf8ToJisTable */
+
 /**
- * @param {Record<string, string>} compressedTable
+ * @param {CompressedTable} compressedTable - target table
+ * @returns {Utf8ToJisTable} decompressed table
  */
 function decompressUtf8ToJisTable (compressedTable) {
   /** @type {Record<number, number>} */
@@ -33,9 +37,7 @@ function decompressUtf8ToJisTable (compressedTable) {
   return result
 }
 
-/**
- * @param {Record<string, string>} compressedTable
- */
+/** @type {(x:CompressedTable) => {Utf8ToJisTable: () => Utf8ToJisTable}} x  */
 export const usingTable = (compressedTable) => {
   let generatedTable = null
   return {
