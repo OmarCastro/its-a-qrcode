5f2294ca9a21e379ecc221a3c5fa2fb1eed700e6
Omar Castro <omar.castro.360@gmail.com>
2023-11-17 21:36:18 +0000

    src: document gif image render

--- src/render/gif-image.render.js
+++ src/render/gif-image.render.js
@@ -8,7 +8,7 @@ import { ByteArrayOutputStream } from '../utils/bite-array-output-stream'
  *
  * @param {number} width - image width
  * @param {number} height - image height
- * @returns
+ * @returns {BWGIFImage} Black and white GIF image
  */
 // eslint-disable-next-line max-lines-per-function
 export function gifImage (width, height) {
@@ -16,109 +16,152 @@ export function gifImage (width, height) {
   const _height = height
   const _data = new Array(width * height).fill(1)
 
-  const _this = {}
+  return {
+    setPixel: (x, y, pixel) => { _data[y * _width + x] = pixel },
+    write (out) {
+      // ---------------------------------
+      // GIF Signature
 
-  /**
-   * @param {number} x - image horizontal position
-   * @param {number} y - image vertical position
-   * @param {number} pixel - pixel color
-   */
-  _this.setPixel = function (x, y, pixel) {
-    _data[y * _width + x] = pixel
+      out.writeString('GIF87a')
+
+      // ---------------------------------
+      // Screen Descriptor
+
+      out.writeShort(_width)
+      out.writeShort(_height)
+
+      out.writeByte(0x80) // 2bit
+      out.writeByte(0)
+      out.writeByte(0)
+
+      // ---------------------------------
+      // Global Color Map
+
+      // black
+      out.writeByte(0x00)
+      out.writeByte(0x00)
+      out.writeByte(0x00)
+
+      // white
+      out.writeByte(0xff)
+      out.writeByte(0xff)
+      out.writeByte(0xff)
+
+      // ---------------------------------
+      // Image Descriptor
+
+      out.writeString(',')
+      out.writeShort(0)
+      out.writeShort(0)
+      out.writeShort(_width)
+      out.writeShort(_height)
+      out.writeByte(0)
+
+      // ---------------------------------
+      // Local Color Map
+
+      // ---------------------------------
+      // Raster Data
+
+      const lzwMinCodeSize = 2
+      const raster = getLZWRaster(_data, lzwMinCodeSize)
+
+      out.writeByte(lzwMinCodeSize)
+
+      let offset = 0
+
+      while (raster.length - offset > 255) {
+        out.writeByte(255)
+        out.writeBytes(raster, offset, 255)
+        offset += 255
+      }
+
+      out.writeByte(raster.length - offset)
+      out.writeBytes(raster, offset, raster.length - offset)
+      out.writeByte(0x00)
+
+      // ---------------------------------
+      // GIF Terminator
+      out.writeString(';')
+    },
   }
+};
 
-  /**
-   *
-   * @param {import('../utils/bite-array-output-stream').ByteArrayOutputStream} out
-   */
-  _this.write = function (out) {
-    // ---------------------------------
-    // GIF Signature
+/**
+ * @param {number[]} data - gif pixel data
+ * @param {number} lzwMinCodeSize - lzw minimum code size
+ */
+function getLZWRaster (data, lzwMinCodeSize) {
+  const clearCode = 1 << lzwMinCodeSize
+  const endCode = (1 << lzwMinCodeSize) + 1
+  let bitLength = lzwMinCodeSize + 1
 
-    out.writeString('GIF87a')
+  // Setup LZWTable
+  const table = lzwTable()
 
-    // ---------------------------------
-    // Screen Descriptor
+  for (let i = 0; i < clearCode; i += 1) {
+    table.add(String.fromCharCode(i))
+  }
+  table.add(String.fromCharCode(clearCode))
+  table.add(String.fromCharCode(endCode))
 
-    out.writeShort(_width)
-    out.writeShort(_height)
+  const byteOut = new ByteArrayOutputStream()
+  const bitOut = bitOutputStream(byteOut)
 
-    out.writeByte(0x80) // 2bit
-    out.writeByte(0)
-    out.writeByte(0)
+  // clear code
+  bitOut.write(clearCode, bitLength)
 
-    // ---------------------------------
-    // Global Color Map
+  let dataIndex = 0
 
-    // black
-    out.writeByte(0x00)
-    out.writeByte(0x00)
-    out.writeByte(0x00)
+  let s = String.fromCharCode(data[dataIndex])
+  dataIndex += 1
 
-    // white
-    out.writeByte(0xff)
-    out.writeByte(0xff)
-    out.writeByte(0xff)
+  while (dataIndex < data.length) {
+    const c = String.fromCharCode(data[dataIndex])
+    dataIndex += 1
 
-    // ---------------------------------
-    // Image Descriptor
+    if (table.contains(s + c)) {
+      s = s + c
+    } else {
+      bitOut.write(table.indexOf(s), bitLength)
 
-    out.writeString(',')
-    out.writeShort(0)
-    out.writeShort(0)
-    out.writeShort(_width)
-    out.writeShort(_height)
-    out.writeByte(0)
+      if (table.size < 0xfff) {
+        if (table.size === (1 << bitLength)) {
+          bitLength += 1
+        }
 
-    // ---------------------------------
-    // Local Color Map
+        table.add(s + c)
+      }
 
-    // ---------------------------------
-    // Raster Data
-
-    const lzwMinCodeSize = 2
-    const raster = getLZWRaster(lzwMinCodeSize)
-
-    out.writeByte(lzwMinCodeSize)
-
-    let offset = 0
-
-    while (raster.length - offset > 255) {
-      out.writeByte(255)
-      out.writeBytes(raster, offset, 255)
-      offset += 255
+      s = c
     }
-
-    out.writeByte(raster.length - offset)
-    out.writeBytes(raster, offset, raster.length - offset)
-    out.writeByte(0x00)
-
-    // ---------------------------------
-    // GIF Terminator
-    out.writeString(';')
   }
 
+  bitOut.write(table.indexOf(s), bitLength)
+
+  // end code
+  bitOut.write(endCode, bitLength)
+
+  bitOut.flush()
+
+  return byteOut.toByteArray()
+}
+
+/** @param {import('../utils/bite-array-output-stream').ByteArrayOutputStream} out - output stream */
+function bitOutputStream (out) {
+  const _out = out
+  let _bitLength = 0
+  let _bitBuffer = 0
+
+  return {
   /**
-   *
-   * @param {import('../utils/bite-array-output-stream').ByteArrayOutputStream} out
+   * @param {number} data - numeric data
+   * @param {number} length - data bit lenght
    */
-  const bitOutputStream = function (out) {
-    const _out = out
-    let _bitLength = 0
-    let _bitBuffer = 0
-
-    const _this = {}
-
-    /**
-     *
-     * @param {number} data
-     * @param {number} length
-     */
-    _this.write = function (data, length) {
+    write (data, length) {
       if ((data >>> length) !== 0) {
         throw Error('length over')
       }
-
       while (_bitLength + length >= 8) {
         _out.writeByte(0xff & ((data << _bitLength) | _bitBuffer))
         length -= (8 - _bitLength)
@@ -129,80 +172,10 @@ export function gifImage (width, height) {
 
       _bitBuffer = (data << _bitLength) | _bitBuffer
       _bitLength = _bitLength + length
-    }
-
-    _this.flush = function () {
-      if (_bitLength > 0) {
-        _out.writeByte(_bitBuffer)
-      }
-    }
-
-    return _this
+    },
+    flush: () => { _bitLength > 0 && _out.writeByte(_bitBuffer) },
   }
-
-  /**
-   *
-   * @param {number} lzwMinCodeSize
-   * @returns
-   */
-  function getLZWRaster (lzwMinCodeSize) {
-    const clearCode = 1 << lzwMinCodeSize
-    const endCode = (1 << lzwMinCodeSize) + 1
-    let bitLength = lzwMinCodeSize + 1
-
-    // Setup LZWTable
-    const table = lzwTable()
-
-    for (let i = 0; i < clearCode; i += 1) {
-      table.add(String.fromCharCode(i))
-    }
-    table.add(String.fromCharCode(clearCode))
-    table.add(String.fromCharCode(endCode))
-
-    const byteOut = new ByteArrayOutputStream()
-    const bitOut = bitOutputStream(byteOut)
-
-    // clear code
-    bitOut.write(clearCode, bitLength)
-
-    let dataIndex = 0
-
-    let s = String.fromCharCode(_data[dataIndex])
-    dataIndex += 1
-
-    while (dataIndex < _data.length) {
-      const c = String.fromCharCode(_data[dataIndex])
-      dataIndex += 1
-
-      if (table.contains(s + c)) {
-        s = s + c
-      } else {
-        bitOut.write(table.indexOf(s), bitLength)
-
-        if (table.size < 0xfff) {
-          if (table.size === (1 << bitLength)) {
-            bitLength += 1
-          }
-
-          table.add(s + c)
-        }
-
-        s = c
-      }
-    }
-
-    bitOut.write(table.indexOf(s), bitLength)
-
-    // end code
-    bitOut.write(endCode, bitLength)
-
-    bitOut.flush()
-
-    return byteOut.toByteArray()
-  }
-
-  return _this
-};
+}
 
 /**
  * @returns {LzwTable} created LZW table
@@ -234,4 +207,11 @@ function lzwTable () {
  * @property {number} size - table size
  * @property {(key: string) => number} indexOf - get index of key
  * @property {(key: string) => boolean} contains - check if key is already in table
+ */
+
+/**
+ * @typedef {object} BWGIFImage
+ * Black and white GIF image
+ * @property {(x: number, y: number, pixel: number) => void} setPixel - set pixel in position
+ * @property {(out: import('../utils/bite-array-output-stream').ByteArrayOutputStream) => void} write - write to output stream
  */
