f81e000856ef2aace66df446ebc011b52a8d896f
Omar Castro <omar.castro.360@gmail.com>
2023-11-25 12:39:59 +0000

    test: add img render test

--- src/render/img-tag.render.js
+++ src/render/img-tag.render.js
@@ -13,6 +13,6 @@ export function createImgTag ({ cellSize, margin, alt, qrcode }) {
   cellSize ||= 2
   margin ??= cellSize * 4
   const size = qrcode.moduleCount * cellSize + margin * 2
-  const altAttr = alt ? `alt="${escapeXml(alt)}"` : ''
-  return `<img src="${createDataURL({ cellSize, margin, qrcode })}" width="${size} height="${size} ${altAttr}/>`
+  const altAttr = alt ? ` alt="${escapeXml(alt)}"` : ''
+  return `<img src="${createDataURL({ cellSize, margin, qrcode })}" width="${size}" height="${size}"${altAttr}/>`
 };
---
+++ src/render/img-tag.render.unit.spec.js
@@ -0,0 +1,12 @@
+import { test } from '../../test-utils/unit/test.util.js'
+import { QrCode } from '../qr-code.js'
+import { createImgTag } from './img-tag.render.js'
+const helloWorldSrc = 'data:image/gif;base64,R0lGODdhOgA6AIAAAAAAAP///ywAAAAAOgA6AAAC/4yPqcvtD6OctNqLs968+w+G4gKU5nkiJYO2JuW6KsDGKEw367Ebe0/KSYAKoDFHTCQdv1tg+RRGfU5ojZqaBmfYl3aIrEpj1G4TZw7PWuVo1rp1e+FneR2cbl+17G+EHtZ0F4enE3ioR/hn86bGx+gFBulYNwkHkjWSkRly1PWp5MjhKQfK5TcqyiMTyslZmETWGlk6EUtJS9qIFtrmOoaIuiiI+wv6OhxMrDtHiyFb1Hy5yRpNrFnTxySlCW34bLnGbVdtG756y2gBmGisCMHu5x7N2wc1SF+xrHoaXy9mKog6c3keARoICyA6XL74PdjXrlnDiQS/raKIadwpih3T1p2zp8whvI8KdwnDhjKlypUsW7p8CTOmTAUFAAA7'
+const helloWorldSvg = `<img src="${helloWorldSrc}" width="58" height="58"/>`
+
+test('Error Correction Polynomial - values are memoized', ({ expect }) => {
+  const qrcode = new QrCode()
+  qrcode.addData('Hello world')
+  qrcode.make()
+  expect(createImgTag({ qrcode })).toEqual(helloWorldSvg)
+})
