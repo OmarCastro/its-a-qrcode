f702f7dc8dd30d2cdd73c17a932eb88993dd5850
Omar Castro <omar.castro.360@gmail.com>
2023-11-27 00:22:07 +0000

    fix: fix transparent margin

--- src/render/canvas-2d-context.render.js
+++ src/render/canvas-2d-context.render.js
@@ -4,12 +4,14 @@
  * @param {CanvasRenderingContext2D} opts.context - canvas rendering context
  * @param {number} [opts.cellSize] - cell size in pixels, defaults to 2
  * @param {import('../qr-code.js').QrCode} opts.qrcode - QR Code data
+ * @param {string} [opts.darkColor] - dark color of QRCode image defaults to black
+ * @param {string} [opts.brightColor] - bright color of QRCode image defaults to white
  */
-export function renderTo2dContext ({ context, cellSize = 2, qrcode }) {
+export function renderTo2dContext ({ context, cellSize = 2, qrcode, darkColor = 'black', brightColor = 'white' }) {
   const length = qrcode.moduleCount
   for (let row = 0; row < length; row++) {
     for (let col = 0; col < length; col++) {
-      context.fillStyle = qrcode.isDark(row, col) ? 'black' : 'white'
+      context.fillStyle = qrcode.isDark(row, col) ? darkColor : brightColor
       context.fillRect(row * cellSize, col * cellSize, cellSize, cellSize)
     }
   }
--- src/render/img-tag.render.unit.spec.js
+++ src/render/img-tag.render.unit.spec.js
@@ -1,7 +1,7 @@
 import { test } from '../../test-utils/unit/test.util.js'
 import { QrCode } from '../qr-code.js'
 import { createImgTag } from './img-tag.render.js'
-const helloWorldSrc = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADoAAAA6CAYAAADhu0ooAAAABmJLR0QA/wD/AP+gvaeTAAABPUlEQVRoge2Z0Q6CMAxFN+P//zI+GIyplNt1ZYnhnDfD2MgOte1oDQAAALL0wJgtM/G2vW/rvZ/+/nmgHnmk41vPLj6ys/4bz+hAz4DFGsmaza7ngVEPbweVgaihqvUsGK3Ci9ms4SwYrcKaq4q5UTDqMbrzNl/aPFq9ngdGLRM16OE8ymzVeju3MSq3bSsKEmVydpkuXoHbGC3Lo6prseO8/Dra7dC9GKTRaH9pr0fnVWbV80TB6E7UkGdaxVyU2Xkw6qEqGWVWofJs1ixGLd4OenlydJ7oaSAxKri81o3mxdHrB2apdVtb8O3ls9Bg7Ro1/CUSo60t+PaiupdsNzMKRj1Gz2WzlUw0RqmMDJef1O9UnRVxwiBYZtSSNUOMCi7/9rIT7VdVHiZGBctrXTXOjo/+W9O9AAAAwDQvbK4NecbxEiUAAAAASUVORK5CYII='
+const helloWorldSrc = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADoAAAA6CAYAAADhu0ooAAAABmJLR0QA/wD/AP+gvaeTAAABP0lEQVRoge2ZQQ7DIAwES5X/f5mefLG0Wts1RIKdW5UESkaOsRlzzvm5gO/bf2AXWuhpaKGnoYWexsNuGGOUBrb0bM+j393zIWTUE91AeSNVs9X5EDKKQG+QGahuqavzeWS0CxSzu4smGe3Cm+uKuSwyisi+eZ8vfR7tng8ho57qHhSNw8x2zWdcY3Ts6gIyk/rqNtGWR1nV4u9D+TVb7bAqyJBRI1pf+uvRcbOGql9jGTWihpBpFnNR/h1HRhFsJ8PMMliejfacPDLqQcZQnsyOE+0GZp83rjG6fK8bzYvZ64pRADXadRbStXdVjBKWn72w6qVazWSRUUS2L1vdo0ZjVF9dx/JOvdHVK1KHgbDNqCd6JuNRjBKWn70Y0XqV5WHFKOG1sxcEy7uKUcK2s5e3ucaoFnoaWuhpaKGn8QMwOyh34UTyBAAAAABJRU5ErkJggg=='
 const helloWorldSvg = `<img src="${helloWorldSrc}" width="58" height="58"/>`
 
 test('Error Correction Polynomial - values are memoized', ({ expect, dom }) => {
