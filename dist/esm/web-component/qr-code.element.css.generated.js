a1f35a83ed257504c2adb071f8a54e760607d367
Omar Castro <omar.castro.360@gmail.com>
2023-12-29 17:58:52 +0000

    dist: prepare next version

--- dist/esm/web-component/qr-code.element.css.generated.js
+++
@@ -1,5 +0,0 @@
-// generated code from src/web-component/qr-code.element.css
-var stdin_default = ":host{display:inline-flex;justify-content:center}svg,img{align-self:center;max-height:100%}\n";
-export {
-  stdin_default as default
-};
