fbfede2aef505c1814afeb33e8ea8375bf641dc0
Omar Castro <omar.castro.360@gmail.com>
2024-01-16 00:38:06 +0000

    dist: prepare next version

--- dist/esm/its-a-qr-code.element.css.generated.js
+++
@@ -1,5 +0,0 @@
-// generated code from src/its-a-qr-code.element.css
-var stdin_default = ":host{display:inline-flex}\n";
-export {
-  stdin_default as default
-};
--- dist/esm/web-component/its-a-qr-code.element.css.generated.js
+++
@@ -1,5 +0,0 @@
-// generated code from src/web-component/its-a-qr-code.element.css
-var stdin_default = ":host{display:inline-flex}\n";
-export {
-  stdin_default as default
-};
