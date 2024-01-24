dca438bae60158fa8b0b3acb5a35c3aaa7c4c0aa
Omar Castro <omar.castro.360@gmail.com>
2024-01-24 22:29:24 +0000

    test: add getCharCountBitLength unit tests

--- src/modes/mode-utils.util.unit.spec.js
+++ src/modes/mode-utils.util.unit.spec.js
@@ -1,5 +1,5 @@
 import { test } from '../../test-utils/unit/test.util.js'
-import { getBestMode } from './mode-utils.util.js'
+import { getBestMode, getCharCountBitLength } from './mode-utils.util.js'
 import { MODE_NUMBER, MODE_ALPHA_NUM, MODE_KANJI, MODE_8BIT_BYTE } from './mode-bits.constants.js'
 
 test('getBestMode - numbers only will set best mode to numeric', ({ expect }) => {
@@ -21,3 +21,42 @@ test('getBestMode - kanji characters will set best mode to kanji', ({ expect }) 
 test('getBestMode - other chars will set best mode to byte', ({ expect }) => {
   expect(getBestMode("Lorem ipsum dolor sit amet").mode).toEqual(MODE_8BIT_BYTE)
 })
+
+test('getCharCountBitLength - invalid type will throw error', ({ expect }) => {
+  expect(() => getCharCountBitLength(MODE_NUMBER, 50)).toThrow(/invalid type: 50/);
+})
+
+test('getCharCountBitLength - invalid mode will throw error', ({ expect }) => {
+  expect(() => getCharCountBitLength(3, 2)).toThrow(/invalid mode: 3/);
+})
+
+test('getCharCountBitLength - validate result respects character count bit length table', ({ expect }) => {
+
+  const charCountTable = [
+    /*             MODE_NUMBER  MODE_ALPHA_NUM MODE_8BIT_BYTE MODE_KANJI */
+    /* 1  to 9  */ 10,          9,             8,             8,
+    /* 10 to 26 */ 12,          11,            16,            10,
+    /* 27 to 40 */ 14,          13,            16,            12,
+  ]
+
+  let colIndex = 0
+  const expectedNumberMode =   [...new Array(9).fill(charCountTable[colIndex]), ...new Array(17).fill(charCountTable[colIndex+4]), ...new Array(14).fill(charCountTable[colIndex+8])]
+  const numberMode = [...Array(40).keys()].map(index => getCharCountBitLength(MODE_NUMBER, index + 1))
+  colIndex++
+  const expectedAlphanumMode = [...new Array(9).fill(charCountTable[colIndex]), ...new Array(17).fill(charCountTable[colIndex+4]), ...new Array(14).fill(charCountTable[colIndex+8])]
+  const alphanumMode = [...Array(40).keys()].map(index => getCharCountBitLength(MODE_ALPHA_NUM, index + 1))
+  colIndex++
+  const expectedbit8Mode = [...new Array(9).fill(charCountTable[colIndex]), ...new Array(17).fill(charCountTable[colIndex+4]), ...new Array(14).fill(charCountTable[colIndex+8])]
+  const bit8Mode = [...Array(40).keys()].map(index => getCharCountBitLength(MODE_8BIT_BYTE, index + 1))
+  colIndex++
+  const expectedKanjiMode = [...new Array(9).fill(charCountTable[colIndex]), ...new Array(17).fill(charCountTable[colIndex+4]), ...new Array(14).fill(charCountTable[colIndex+8])]
+  const kanjiMode = [...Array(40).keys()].map(index => getCharCountBitLength(MODE_KANJI, index + 1))
+
+  expect({numberMode, alphanumMode, bit8Mode, kanjiMode}).toEqual({
+    numberMode: expectedNumberMode, 
+    alphanumMode: expectedAlphanumMode, 
+    bit8Mode: expectedbit8Mode, 
+    kanjiMode: expectedKanjiMode
+  })
+})
+
