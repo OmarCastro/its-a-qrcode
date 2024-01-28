0c5991f10e70cf35718f3c032b0d081fb92948eb
Omar Castro <omar.castro.360@gmail.com>
2024-01-28 12:52:49 +0000

    src: enable anti-aliasing in SVG due to dot rendering

--- src/render/svg.render.js
+++ src/render/svg.render.js
@@ -31,7 +31,6 @@ export function createSvgTag ({ cellSize, margin, alt, title, qrcode, scalable, 
   let qrSvg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg"'
   qrSvg += !scalable ? ' width="' + size + 'px" height="' + size + 'px"' : ''
   qrSvg += ' viewBox="0 0 ' + size + ' ' + size + '" preserveAspectRatio="xMinYMin meet"'
-  qrSvg += ' shape-rendering="crispEdges"' // disables anti-aliasing, without it, a line between the QR Code paint area and the margin will appear
   qrSvg += a11yAttributes(titleProp, altProp) + '>'
   qrSvg += (titleProp.text) ? '<title id="' + escapeXml(titleProp.id) + '">' + escapeXml(titleProp.text) + '</title>' : ''
   qrSvg += (altProp.text) ? '<description id="' + escapeXml(altProp.id) + '">' + escapeXml(altProp.text) + '</description>' : ''
