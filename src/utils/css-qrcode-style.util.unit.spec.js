a4768e1e2b2121545d6e3d147cce53b040e07a15
OmarCastro <omar.castro.360@gmail.com>
2024-02-03 18:21:48 +0000

    test: add css qrcode style validity unit test

--- src/utils/css-qrcode-style.util.unit.spec.js
+++ src/utils/css-qrcode-style.util.unit.spec.js
@@ -11,7 +11,7 @@ test('parseQrCodeStyles - 3 valid style on --qrcode-style will apply all styles 
   })
 })
 
-test('parseQrCodeStyles - style is case insensitive ', ({ expect, dom }) => {
+test('parseQrCodeStyles - --qrcode-style is case insensitive ', ({ expect, dom }) => {
   expect(parseQrCodeStyles({
     style: 'DoT dOt RoUnDeD',
   })).toEqual({
@@ -19,4 +19,37 @@ test('parseQrCodeStyles - style is case insensitive ', ({ expect, dom }) => {
     cornerBorder: 'dot',
     cornerCenter: 'rounded',
   })
+})
+
+
+test('parseQrCodeStyles - 2 valid colors on --qrcode-style will apply the style on all dots then on corner border only', ({ expect, dom }) => {
+  expect(parseQrCodeStyles({ 
+    style: "square dOt"
+   })).toEqual({
+    dots: 'square',
+    cornerBorder: 'dot',
+    cornerCenter: 'default',
+   })
+})
+
+
+test('parseQrCodeStyles - 1 valid color on --qrcode-style will apply the style on all dots only', ({ expect, dom }) => {
+  expect(parseQrCodeStyles({ 
+    style: "rounded"
+   })).toEqual({
+    dots: 'rounded',
+    cornerBorder: 'default',
+    cornerCenter: 'default',
+   })
+})
+
+
+test('parseQrCodeStyles - all invalid styles on --qrcode-style will apply the default styles', ({ expect, dom }) => {
+  expect(parseQrCodeStyles({ 
+    style: "invalid-style invalid-style invalid-style"
+   })).toEqual({
+    dots: 'default',
+    cornerBorder: 'default',
+    cornerCenter: 'default',
+   })
 })
