9bce384c4c141ce074f34078c7fece0787a63172
OmarCastro <omar.castro.360@gmail.com>
2024-09-21 14:36:32 +0100

    build: update dev dependencies

--- buildfiles/run.js
+++ buildfiles/run.js
@@ -838,25 +838,32 @@ async function getFilesAsArray (dir) {
   return arr
 }
 /**
- *
+ * Watch firectories for file changes
  * @param  {...string} dirs
  * @yields {Promise<{filenames: string[]}>}
  * @returns {AsyncGenerator<Promise<{filenames: string[]}>>} iterator of changed filenames
  */
 async function * watchDirs (...dirs) {
-  const { watch } = await import('chokidar')
+  const { watch } = await import('node:fs')
   const nothingResolver = () => {}
   let currentResolver = nothingResolver
   let batch = []
   console.log(`watching ${dirs}`)
-  watch(dirs).on('change', (filename) => {
+
+  /** @type {import('node:fs').WatchListener<string>} */
+  const handler = (eventType, filename) => {
+    if (eventType !== 'change' || filename == null) { return }
     batch.push(filename)
     if (currentResolver !== nothingResolver) {
       currentResolver({ filenames: batch })
       batch = []
       currentResolver = nothingResolver
     }
-  })
+  }
+  for (const dir of dirs) {
+    watch(dir, { recursive: true }, handler)
+  }
+
   while (true) {
     yield new Promise(resolve => {
       if (batch.length > 0) {
