57a08d2d1a2ac739ca1238dae97b8ce5a02d5a8e
OmarCastro <omar.castro.360@gmail.com>
2024-10-11 00:29:58 +0100

    release: prepare next version

--- dist/esm/entrypoint/browser.js
+++
@@ -1,7 +0,0 @@
-import { QRCodeElement as Element } from '../web-component/qr-code.element.js'
-export { QrCode } from '../qr-code.js'
-const url = new URL(import.meta.url)
-const tagName = url.searchParams.get('named')?.trim()
-tagName && customElements.define(tagName, Element)
-export const QRCodeElement = Element
-export default QRCodeElement
