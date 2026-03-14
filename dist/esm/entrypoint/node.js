0c72cedd8b1ff8128b97fcad47a20882fa15692f
OmarCastro <omar.castro.360@gmail.com>
2026-03-14 13:00:23 +0000

    release: cleanup

--- dist/esm/entrypoint/node.js
+++
@@ -1,4 +0,0 @@
-import { QRCodeElement as Element } from '../web-component/qr-code.element.js'
-export { QrCode } from '../qr-code.js'
-export const QRCodeElement = Element
-export default QRCodeElement
