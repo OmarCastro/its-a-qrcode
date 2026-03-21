cf459fe9771ed0849f0ebaf54d3e47852a76dc06
OmarCastro <omar.castro.360@gmail.com>
2026-03-21 13:54:32 +0000

    build: use custom arrow parens style

--- buildfiles/configs/eslint.stylistic.config.js
+++ buildfiles/configs/eslint.stylistic.config.js
@@ -12,10 +12,11 @@ export default [
   {
     plugins: {
       '@stylistic/js': stylisticJs,
+      '@custom': customPlugin(),
     },
 
     rules: {
-      '@stylistic/js/arrow-parens': ['error'],
+      '@custom/arrow-parens': ['error'],
       '@stylistic/js/arrow-spacing': 'error',
       '@stylistic/js/block-spacing': 'error',
       '@stylistic/js/brace-style': ['error', '1tbs', { allowSingleLine: true }],
@@ -77,3 +78,87 @@ export default [
     },
   },
 ]
+
+
+function customPlugin () {
+
+  /**
+   * Checks whether or not a given token is `(`.
+   * @param {Token} token - A token to check.
+   * @returns {boolean} `true` when the token is `(`.
+   */
+  function isOpenParen (token) {
+    return token.type === 'Punctuator' && token.value === '('
+  }
+
+  /**
+   * Checks whether or not given two tokens are at a same line.
+   * @param {Token} a - A left token.
+   * @param {Token} b - A right token.
+   * @returns {boolean} `true` when the tokens are at a same line.
+   */
+  function isSameLine (a, b) {
+    return a.loc.end.line === b.loc.start.line
+  }
+
+  return {
+    rules: {
+      'arrow-parens': {
+        meta: {
+          docs: {
+            description: 'enforce the parentheses style of arrow functions.',
+            category: 'Stylistic Issues',
+            recommended: false,
+          },
+          fixable: 'code',
+          schema: [],
+          type: 'suggestion',
+        },
+        create (context) {
+          return {
+            ArrowFunctionExpression (node) {
+              const first = context
+                    .getSourceCode()
+                    .getFirstToken(node, node.async ? 1 : 0)
+              const before = context.getSourceCode().getTokenBefore(first)
+
+              if (isOpenParen(first)) {
+                if (
+                  node.params.length === 1 &&
+                  node.params[0].type === 'Identifier' &&
+                  isOpenParen(before) &&
+                  isSameLine(before, first)
+                ) {
+                  context.report({
+                    node,
+                    message: 'Unexpected parentheses enclosing this argument.',
+                    fix (fixer) {
+                      const id = node.params[0]
+                      const begin = first.range[0]
+                      const end = context
+                                    .getSourceCode()
+                                    .getTokenAfter(id).range[1]
+
+                      return fixer.replaceTextRange([begin, end], id.name)
+                    },
+                  })
+                }
+              } else if (!isOpenParen(before) || !isSameLine(before, first)) {
+                context.report({
+                  node,
+                  message: 'Expected to enclose this argument with parentheses.',
+                  fix (fixer) {
+                    const id = node.params[0]
+
+                    return fixer.replaceText(id, `(${id.name})`)
+                  },
+                })
+              }
+            },
+          }
+        },
+      },
+
+    },
+  }
+}
